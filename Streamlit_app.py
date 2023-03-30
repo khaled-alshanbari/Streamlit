6d32b38d5ac1f90f73ddfc70f47e156e83da0437
khaled <83522218+khaled-alshanbari@users.noreply.github.com>
2023-03-30 03:46:16 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -5,6 +5,6 @@ st.set_page_config(page_title="AirForce Security", layout='wide')
 
 connection = create_db_connection()
 
-out = read_query(connection, "select * from streamlit"):
+out = read_query(connection, "select * from streamlit")
 
 st.write(out)
