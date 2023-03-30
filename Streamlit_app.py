9b82704c47bcabec2a0a8ff74f682f38e6e22542
khaled <83522218+khaled-alshanbari@users.noreply.github.com>
2023-03-30 03:45:49 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit as st
 from SQLConnector import *
 st.set_page_config(page_title="AirForce Security", layout='wide')
 
-st.write("Hello")
+connection = create_db_connection()
 
+out = read_query(connection, "select * from streamlit"):
 
+st.write(out)
